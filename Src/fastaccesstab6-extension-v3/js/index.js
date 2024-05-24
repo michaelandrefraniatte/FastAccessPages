4cf0e105158e812bf93156939e1d915457eb1d62
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-05-24 12:31:43 +0200

    Fix a bug

--- Src/fastaccesstab6-extension-v3/js/index.js
+++ Src/fastaccesstab6-extension-v3/js/index.js
@@ -1,16 +1,32 @@
 
 document.getElementById("submit").addEventListener("click", function (e) {
+    search();
+});
+
+document.getElementById("search").addEventListener("keydown", function (e) {
+    if (e.keyCode == 13) {
+        search();
+    }
+});
+
+function search() {
     var searchterm = document.getElementById("search").value;
     window.location.href = "https://www.google.com/search?q=" + escape(searchterm);
-});
+}
 
 function escape(unsafe) {
     return encodeURIComponent(unsafe);
 }
 
-document.getElementById("search").addEventListener("keydown", function (e) {
-    if (e.keyCode == 13) {
-        var searchterm = document.getElementById("search").value;
-        window.location.href = "https://www.google.com/search?q=" + escape(searchterm);
-    }
-});
+function boxClick() {
+    window.location.href = this.title;
+}
+
+const elements = document.getElementsByClassName("box");
+for (let i = 0; i < elements.length; i++) {
+    elements[i].setAttribute('draggable', false);
+    elements[i].addEventListener("dragover", (event) => { allowDrop(event); });
+    elements[i].addEventListener("dragstart", (event) => { drag(event); });
+    elements[i].addEventListener("drop", (event) => { drop(event); });
+    elements[i].onclick = boxClick;
+}
